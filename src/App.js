04aef58d5672de60fcbce5e2e0edfb34ac915dde
Rheanne Reyes <rheannemr@gmail.com>
2021-05-23 22:55:51 -0700

    Add pages

--- src/App.js
+++ src/App.js
@@ -1,1 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route } from "react-router-dom";
+
+function App() {
+    return (
+        <BrowserRouter>
+        <Route>
+            
+        </Route>
+        </BrowserRouter>
+    );
+}
+
+export default App;
 
