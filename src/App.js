ac91e0536bab463252340736a660eccd113c7325
Rheanne Reyes <rheannemr@gmail.com>
2021-05-24 12:56:20 -0700

    Add page routes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import { BrowserRouter, Route } from "react-router-dom";
+import Landing from "./pages/Landing";
+import About from "./pages/About";
+import Work from "./pages/Work";
+import Contact from "./pages/Contact";
 
 function App() {
     return (
         <BrowserRouter>
-        <Route>
-            
-        </Route>
+        <Route exact path ="/" component={Landing} />
+        <Route exact path ="/about" component={About} />
+        <Route exact path ="/work" component={Work} />
+        <Route exact path ="/contact" component={Contact} />
         </BrowserRouter>
     );
 }
