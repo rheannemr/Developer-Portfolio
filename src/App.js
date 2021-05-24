05beb2d3a4513893e72bcf35faf9739d18c10fe8
Rheanne Reyes <rheannemr@gmail.com>
2021-05-24 13:00:18 -0700

    Fix router bug

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Route } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import Landing from "./pages/Landing";
 import About from "./pages/About";
 import Work from "./pages/Work";
@@ -8,10 +8,12 @@ import Contact from "./pages/Contact";
 function App() {
     return (
         <BrowserRouter>
+        <Switch>
         <Route exact path ="/" component={Landing} />
         <Route exact path ="/about" component={About} />
         <Route exact path ="/work" component={Work} />
         <Route exact path ="/contact" component={Contact} />
+        </Switch>
         </BrowserRouter>
     );
 }
